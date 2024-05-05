48c9d8e25bddc5434c2359170ab39b7abb66018f
Alex Marques Beraldo <alexmarquesberaldo@gmail.com>
2024-05-05 16:42:27 -0300

    Desafio finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,15 +1,43 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    private Scanner scan = new Scanner(System.in);
-    private int numero;
-    private String agencia, nomeCliente;
-    private float saldo;
 
     public static void main(String[] args) throws Exception {
+        int numero;
+        String nomeAgencia, nomeCliente;
+        float saldo = 0;
 
+        //TODO:Conhecer e importar a classe Scanner
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, " +
-                "sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque");
+        System.out.println("Por favor digite o número da conta!");
+        if (scan.hasNextInt()) {
+            numero = scan.nextInt();
+            // Consumir a quebra de linha após a entrada do número
+            scan.nextLine();
+        } else {
+            System.out.println("Entrada inválida para o número da conta.");
+            return;
+        }
+
+        System.out.println("Por favor, digite o nome da agência");
+        nomeAgencia = scan.nextLine();
+
+        System.out.println("Por favor, digite seu nome completo");
+        nomeCliente = scan.nextLine();
+
+        System.out.println("Seu saldo inicial é R$ 0,00, digite o valor que deseja adicionar");
+        if (scan.hasNextFloat()) {
+            saldo += scan.nextFloat();
+            // Consumir a quebra de linha após a entrada do valor do saldo
+            scan.nextLine();
+        } else {
+            System.out.println("Entrada inválida para o saldo inicial.");
+            return;
+        }
+
+        //TODO:exibir a mensagem conta criada
+        System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é "+ nomeAgencia +", conta "+numero+" e seu saldo R$ "+saldo+" já está disponível para saque");
     }
 }
